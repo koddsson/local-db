f86cbf6074c7093d51204d05c8710a63a258afe3
Kristján Oddsson <koddsson@gmail.com>
2024-05-02 22:12:17 +0200

    init

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,44 @@
-// Put the code here
-export function add(a: number, b: number) {
-  return a + b;
+export class DB<T> {
+  #key: string;
+
+  constructor(key: string) {
+    this.#key = key;
+  }
+
+  find(partial: Partial<T>): T | undefined {
+    return this.all.find((x) =>
+      Object.entries(partial).every(([key, value]) => {
+        // @ts-expect-error I'm not sure.
+        return x[key] === value;
+      }),
+    );
+  }
+
+  remove(partial: Partial<T>) {
+    const filtered = this.all.filter(
+      (x) =>
+        !Object.entries(partial).every(([key, value]) => {
+          // @ts-expect-error I'm not sure.
+          return x[key] === value;
+        }),
+    );
+    this.#save(filtered);
+  }
+
+  insert(item: T) {
+    this.#save([...this.all, item]);
+  }
+
+  #save(items: T[]) {
+    const encoded = JSON.stringify(items);
+    window.localStorage.setItem(this.#key, encoded);
+  }
+
+  removeAll() {
+    window.localStorage.setItem(this.#key, '[]');
+  }
+
+  get all(): T[] {
+    return JSON.parse(window.localStorage.getItem(this.#key) || '[]').sort();
+  }
 }
--- tests/index.ts
+++ tests/index.ts
@@ -1,7 +1,70 @@
-import {assert} from '@open-wc/testing'
+import { expect } from '@open-wc/testing';
+import { faker } from '@faker-js/faker';
+import { DB } from '../src/js/index';
 
-describe('my tests', () => {
-  it('adds two numbers', () => {
-    assert.equal(2 + 2, 4)
-  })
-})
+interface TestDatum {
+  name: string;
+  url: string;
+}
+
+function id() {
+  return faker.string.alphanumeric(5);
+}
+
+function datum() {
+  return {
+    name: faker.person.fullName(),
+    url: faker.internet.url({ protocol: 'https' }),
+  };
+}
+
+describe('the db', () => {
+  let db: DB<TestDatum>;
+  beforeEach(() => {
+    db = new DB(id());
+  });
+
+  afterEach(() => {
+    db.removeAll();
+  });
+
+  it('starts off empty', () => {
+    expect(db.all).to.be.empty;
+  });
+
+  it('can insert datum', () => {
+    const item = datum();
+    db.insert(item);
+    expect(db.all).to.deep.equal([item]);
+  });
+
+  it('can find specific datum', () => {
+    const items = [datum(), datum(), datum()];
+    db.insert(items[0]);
+    db.insert(items[1]);
+    db.insert(items[2]);
+    expect(db.all).to.deep.equal(items);
+    const found = db.find({ name: items[1].name });
+    expect(found).to.deep.equal(items[1]);
+  });
+
+  it('can remove specific datum', () => {
+    const items = [datum(), datum(), datum()];
+    db.insert(items[0]);
+    db.insert(items[1]);
+    db.insert(items[2]);
+    expect(db.all).to.deep.equal(items);
+    db.remove({ name: items[1].name });
+    expect(db.all).to.deep.equal([items[0], items[2]]);
+  });
+
+  it('can remove all data', () => {
+    const items = [datum(), datum(), datum()];
+    db.insert(items[0]);
+    db.insert(items[1]);
+    db.insert(items[2]);
+    expect(db.all).to.deep.equal(items);
+    db.removeAll();
+    expect(db.all).to.be.empty;
+  });
+});

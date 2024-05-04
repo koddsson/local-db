06dfca0dc365e7eca572e580cc7a7d14425815d2
Kristján Oddsson <koddsson@gmail.com>
2024-05-04 09:41:36 +0200

    Publish TypeScript declarations

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-import postcssJitProps from 'postcss-jit-props';
-import OpenProps from 'open-props';
-import atImport from 'postcss-import';
-import nano from 'cssnano';
-
-export default {
-  plugins: [atImport(), postcssJitProps(OpenProps), nano({preset: 'default'})]
-}
